6a20c56243669861179cdd88cce23aa4944b4bfd
arthur <mdpbriar@henallux.be>
2026-05-25 16:21:10 +0200

    feat: ajout préposition pour organe

--- core/models/organe.py
+++ core/models/organe.py
@@ -8,6 +8,13 @@ class Organe(models.Model):
 
     nom_court = models.CharField(max_length=100, blank=True, null=True)
 
+    preposition = models.CharField(
+        max_length=20,
+        blank=True,
+        default='',
+        help_text="Préposition à utiliser devant le nom dans les documents (ex: \"au\", \"à l'\", \"à la\").",
+    )
+
     duree_mandat = models.PositiveIntegerField(default=0, help_text="Durée par défaut d'un mandat, en années (0 = pas de date de fin)")
 
     membres = models.ManyToManyField(
